8eb7694acff266445cffe1e1b473bcf90040b433
Jorge Calás Lozano <calas@railsdog.com>
2009-10-28 13:58:34 +0100

    Adding javascript functionality

---
+++ public/javascripts/product_comparison.js
@@ -0,0 +1,19 @@
+function disableIf(query, value){ $(query).attr('disabled', value); }
+
+function countChecked() { 
+    var checkedProducts = $(':checkbox.compare:checked').length;
+    disableIf(':checkbox.compare:unchecked', (checkedProducts >= 4));
+    disableIf("form[action*='/compare_products'] :submit", (checkedProducts < 2));
+}
+
+function directSubmit() {
+    var product_id = $.map($(':checkbox.compare:checked'), function(n) {return n.value;}).join('/');
+    window.location = '/t/' + $('#taxon')[0].value + '/compare/' + product_id;
+    return false;
+};
+
+$(function(){
+      countChecked();
+      $(":checkbox").click(countChecked);
+      $("form[action*='/compare_products']").submit(directSubmit);
+  });
