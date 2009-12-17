f86a50dad06419d4b9f5c9ab432471f521b637cf
David North <davidnorth@railsdog.com>
2009-12-17 21:34:55 +0000

    update hooks to new erb style

--- compare_products_hooks.rb
+++ compare_products_hooks.rb
@@ -1,11 +1,11 @@
 class CompareProductsHooks < Spree::ThemeSupport::HookListener
   
   replace :taxon_products do
-    if @taxon && @taxon.taxonomy.comparable? && @products.length > 1
-      render('taxons/comparable_taxon')
-    else
-      render("shared/products.html.erb", :products => @products, :taxon => @taxon)
-    end
+    '<% if @taxon && @taxon.taxonomy.comparable? && @products.length > 1 %>
+      <%= render("taxons/comparable_taxon") %>
+    <% else %>
+      <%= render("shared/products.html.erb", :products => @products, :taxon => @taxon) %>
+    <% end %>'
   end
   
   insert_after :admin_inside_taxonomy_form, 'admin/taxonomies/comparable_field'
